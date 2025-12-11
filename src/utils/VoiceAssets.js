be5467405d5719e00acacee257b01dfb66e68375
pdkokare1 <pdkokare1@gmail.com>
2025-12-11 17:36:11 +0530

    Update VoiceAssets.js

--- src/utils/VoiceAssets.js
+++ src/utils/VoiceAssets.js
@@ -1,71 +1,76 @@
 // src/utils/VoiceAssets.js
 
+const CLOUD_BASE = "https://res.cloudinary.com/dphvvutwj/video/upload/the-gamut-audio";
+
 export const VOICE_ASSETS = {
-  // --- SESSION OPENERS (Live) ---
+  // --- SESSION OPENERS ---
   OPENERS: {
     MIRA: {
       MORNING: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442263/the-gamut-audio/mira_open_morn_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442266/the-gamut-audio/mira_open_morn_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442270/the-gamut-audio/mira_open_morn_3.mp3"
+        `${CLOUD_BASE}/mira_open_morn_1.mp3`, `${CLOUD_BASE}/mira_open_morn_2.mp3`, `${CLOUD_BASE}/mira_open_morn_3.mp3`
       ],
       AFTERNOON: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442274/the-gamut-audio/mira_open_aft_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442278/the-gamut-audio/mira_open_aft_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442281/the-gamut-audio/mira_open_aft_3.mp3"
+        `${CLOUD_BASE}/mira_open_aft_1.mp3`, `${CLOUD_BASE}/mira_open_aft_2.mp3`, `${CLOUD_BASE}/mira_open_aft_3.mp3`
       ],
       EVENING: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442285/the-gamut-audio/mira_open_eve_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442289/the-gamut-audio/mira_open_eve_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442293/the-gamut-audio/mira_open_eve_3.mp3"
+        `${CLOUD_BASE}/mira_open_eve_1.mp3`, `${CLOUD_BASE}/mira_open_eve_2.mp3`, `${CLOUD_BASE}/mira_open_eve_3.mp3`
       ]
     },
     RAJAT: {
       MORNING: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442297/the-gamut-audio/rajat_open_morn_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442301/the-gamut-audio/rajat_open_morn_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442305/the-gamut-audio/rajat_open_morn_3.mp3"
+        `${CLOUD_BASE}/rajat_open_morn_1.mp3`, `${CLOUD_BASE}/rajat_open_morn_2.mp3`, `${CLOUD_BASE}/rajat_open_morn_3.mp3`
       ],
       AFTERNOON: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442309/the-gamut-audio/rajat_open_aft_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442315/the-gamut-audio/rajat_open_aft_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442319/the-gamut-audio/rajat_open_aft_3.mp3"
+        `${CLOUD_BASE}/rajat_open_aft_1.mp3`, `${CLOUD_BASE}/rajat_open_aft_2.mp3`, `${CLOUD_BASE}/rajat_open_aft_3.mp3`
       ],
       EVENING: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442323/the-gamut-audio/rajat_open_eve_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442327/the-gamut-audio/rajat_open_eve_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442330/the-gamut-audio/rajat_open_eve_3.mp3"
+        `${CLOUD_BASE}/rajat_open_eve_1.mp3`, `${CLOUD_BASE}/rajat_open_eve_2.mp3`, `${CLOUD_BASE}/rajat_open_eve_3.mp3`
       ]
     },
     SHUBHI: {
       MORNING: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442334/the-gamut-audio/shubhi_open_morn_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442338/the-gamut-audio/shubhi_open_morn_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442341/the-gamut-audio/shubhi_open_morn_3.mp3"
+        `${CLOUD_BASE}/shubhi_open_morn_1.mp3`, `${CLOUD_BASE}/shubhi_open_morn_2.mp3`, `${CLOUD_BASE}/shubhi_open_morn_3.mp3`
       ],
       AFTERNOON: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442346/the-gamut-audio/shubhi_open_aft_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442352/the-gamut-audio/shubhi_open_aft_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442356/the-gamut-audio/shubhi_open_aft_3.mp3"
+        `${CLOUD_BASE}/shubhi_open_aft_1.mp3`, `${CLOUD_BASE}/shubhi_open_aft_2.mp3`, `${CLOUD_BASE}/shubhi_open_aft_3.mp3`
       ],
       EVENING: [
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442361/the-gamut-audio/shubhi_open_eve_1.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442366/the-gamut-audio/shubhi_open_eve_2.mp3",
-        "https://res.cloudinary.com/dphvvutwj/video/upload/v1765442371/the-gamut-audio/shubhi_open_eve_3.mp3"
+        `${CLOUD_BASE}/shubhi_open_eve_1.mp3`, `${CLOUD_BASE}/shubhi_open_eve_2.mp3`, `${CLOUD_BASE}/shubhi_open_eve_3.mp3`
       ]
     }
   },
 
-  // --- PLACEHOLDERS (To be generated next) ---
-  WELCOME_BACK: {
-    MIRA: [], RAJAT: [], SHUBHI: []
+  // --- SEGUES (Same Host Transitions) ---
+  SEGUES: {
+    MIRA: [
+      `${CLOUD_BASE}/mira_segue_01.mp3`, `${CLOUD_BASE}/mira_segue_02.mp3`, `${CLOUD_BASE}/mira_segue_03.mp3`,
+      `${CLOUD_BASE}/mira_segue_04.mp3`, `${CLOUD_BASE}/mira_segue_05.mp3`, `${CLOUD_BASE}/mira_segue_06.mp3`,
+      `${CLOUD_BASE}/mira_segue_07.mp3`, `${CLOUD_BASE}/mira_segue_08.mp3`, `${CLOUD_BASE}/mira_segue_09.mp3`,
+      `${CLOUD_BASE}/mira_segue_10.mp3`
+    ],
+    RAJAT: [
+      `${CLOUD_BASE}/rajat_segue_01.mp3`, `${CLOUD_BASE}/rajat_segue_02.mp3`, `${CLOUD_BASE}/rajat_segue_03.mp3`,
+      `${CLOUD_BASE}/rajat_segue_04.mp3`, `${CLOUD_BASE}/rajat_segue_05.mp3`, `${CLOUD_BASE}/rajat_segue_06.mp3`,
+      `${CLOUD_BASE}/rajat_segue_07.mp3`, `${CLOUD_BASE}/rajat_segue_08.mp3`, `${CLOUD_BASE}/rajat_segue_09.mp3`,
+      `${CLOUD_BASE}/rajat_segue_10.mp3`
+    ],
+    SHUBHI: [
+      `${CLOUD_BASE}/shubhi_segue_01.mp3`, `${CLOUD_BASE}/shubhi_segue_02.mp3`, `${CLOUD_BASE}/shubhi_segue_03.mp3`,
+      `${CLOUD_BASE}/shubhi_segue_04.mp3`, `${CLOUD_BASE}/shubhi_segue_05.mp3`, `${CLOUD_BASE}/shubhi_segue_06.mp3`,
+      `${CLOUD_BASE}/shubhi_segue_07.mp3`, `${CLOUD_BASE}/shubhi_segue_08.mp3`, `${CLOUD_BASE}/shubhi_segue_09.mp3`,
+      `${CLOUD_BASE}/shubhi_segue_10.mp3`
+    ]
   },
+
+  // --- HANDOVERS (Placeholders for next batch) ---
   HANDOVERS: {
     MIRA_TO_RAJAT: [], MIRA_TO_SHUBHI: [],
     RAJAT_TO_MIRA: [], RAJAT_TO_SHUBHI: [],
     SHUBHI_TO_MIRA: [], SHUBHI_TO_RAJAT: []
   },
-  SEGUES: {
+  
+  // --- WELCOME BACK (Placeholders) ---
+  WELCOME_BACK: {
     MIRA: [], RAJAT: [], SHUBHI: []
   }
 };

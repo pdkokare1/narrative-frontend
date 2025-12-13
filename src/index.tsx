c7c6be376a2b9a57d084301e2d14944d870dc475
pdkokare1 <pdkokare1@gmail.com>
2025-12-13 17:05:24 +0530

    Update and rename index.js to index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+// src/index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.register();

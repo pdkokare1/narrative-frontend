4c53d4ceaf6093884ca9ddfae5a8fc2f6bbe1fc8
pdkokare1 <pdkokare1@gmail.com>
2025-12-14 10:56:23 +0530

    Update api.tsx

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -117,6 +117,7 @@ export const getProfile = () => api.get<IUserProfile>('/profile/me');
 export const getStats = () => api.get<any>('/profile/stats');
 export const getWeeklyDigest = () => api.get<any>('/profile/weekly-digest');
 export const createProfile = (data: { username: string }) => api.post<IUserProfile>('/profile', data);
+export const deleteAccount = () => api.delete('/profile'); // NEW: Update 9
 
 // Notification Token
 export const saveNotificationToken = (token: string) => api.post('/profile/save-token', { token });

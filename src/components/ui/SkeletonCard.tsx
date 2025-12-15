2a325e18418c23eee94f0b0734c9fc60c0e4164b
pdkokare1 <pdkokare1@gmail.com>
2025-12-15 22:40:22 +0530

    Update SkeletonCard.tsx

--- src/components/ui/SkeletonCard.tsx
+++ src/components/ui/SkeletonCard.tsx
@@ -1,40 +1,49 @@
 // src/components/ui/SkeletonCard.tsx
 import React from 'react';
 import './SkeletonCard.css';
-import '../../App.css'; // For the global shimmer/pulse animation
+import '../../App.css'; 
 
 const SkeletonCard: React.FC = () => {
   return (
     <div className="skeleton-card">
+      {/* Badges Placeholder */}
+      <div className="skel-badges">
+        <div className="skel-badge skeleton-pulse"></div>
+      </div>
+
       {/* Image Placeholder */}
       <div className="skel-image-wrapper">
         <div className="skel-fill skeleton-pulse"></div>
       </div>
 
       <div className="skel-content">
-        {/* Meta Row (Source & Date) */}
+        {/* Meta Row */}
         <div className="skel-meta-row">
-          <div className="skel-block skel-btn skeleton-pulse" style={{ width: '80px' }}></div>
-          <div className="skel-block skel-btn skeleton-pulse" style={{ width: '60px' }}></div>
+          <div className="skel-block skeleton-pulse" style={{ width: '80px', height: '10px' }}></div>
+          <div className="skel-block skeleton-pulse" style={{ width: '60px', height: '10px' }}></div>
         </div>
 
         {/* Headline */}
-        <div className="skel-title skel-block skeleton-pulse"></div>
-        <div className="skel-title-sub skel-block skeleton-pulse" style={{ width: '80%' }}></div>
+        <div>
+            <div className="skel-title skel-block skeleton-pulse"></div>
+            <div className="skel-title-sub skel-block skeleton-pulse"></div>
+        </div>
 
-        {/* Summary Lines */}
-        <div className="skel-line skel-block skeleton-pulse"></div>
-        <div className="skel-line skel-block skeleton-pulse"></div>
-        <div className="skel-line skel-block skeleton-pulse skel-line-last" style={{ width: '60%' }}></div>
+        {/* Summary */}
+        <div className="skel-summary-block">
+            <div className="skel-line skel-block skeleton-pulse"></div>
+            <div className="skel-line skel-block skeleton-pulse"></div>
+            <div className="skel-line skel-line-short skel-block skeleton-pulse"></div>
+        </div>
 
-        {/* Footer Actions */}
+        {/* Footer */}
         <div className="skel-footer">
           <div className="skel-actions-left">
-            <div className="skel-icon skel-block skeleton-pulse"></div>
-            <div className="skel-icon skel-block skeleton-pulse"></div>
-            <div className="skel-icon skel-block skeleton-pulse"></div>
+            <div className="skel-icon skeleton-pulse"></div>
+            <div className="skel-icon skeleton-pulse"></div>
+            <div className="skel-icon skeleton-pulse"></div>
           </div>
-          <div className="skel-btn skel-block skeleton-pulse" style={{ width: '80px' }}></div>
+          <div className="skel-text-btn skel-block skeleton-pulse"></div>
         </div>
       </div>
     </div>

4ce9e0679c3ea78c1a800783aff2746a1a299397
pdkokare1 <pdkokare1@gmail.com>
2025-12-16 11:43:10 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,26 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.thegamut.app',
+  appName: 'The Gamut',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#121212",
+      showSpinner: false,
+      androidSplashResourceName: "splash",
+      iosSplashResourceName: "Default-568h@2x~iphone"
+    },
+    StatusBar: {
+      style: "DARK",
+      backgroundColor: "#121212"
+    }
+  }
+};
+
+export default config;
+}

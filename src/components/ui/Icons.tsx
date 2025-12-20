d3f1bc5bf83fb8c631cdf4ecd070594e5de3d689
pdkokare1 <pdkokare1@gmail.com>
2025-12-21 03:37:21 +0530

    Update Icons.tsx

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -88,3 +88,11 @@ export const ChevronDownIcon = (props: IconProps) => (
     <polyline points="6 9 12 15 18 9"></polyline>
   </BaseIcon>
 );
+
+export const RefreshIcon = (props: IconProps) => (
+    <BaseIcon {...props}>
+        <path d="M23 4v6h-6"></path>
+        <path d="M1 20v-6h6"></path>
+        <path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path>
+    </BaseIcon>
+);

4cdfbfcf1945811c9978af52e009bba995c8a7f2
pdkokare1 <pdkokare1@gmail.com>
2026-01-01 11:04:40 +0530

    Update FeedItemRenderer.tsx

--- src/components/feeds/FeedItemRenderer.tsx
+++ src/components/feeds/FeedItemRenderer.tsx
@@ -1,69 +1,58 @@
+// src/components/feeds/FeedItemRenderer.tsx
 import React from 'react';
 import ArticleCard from '../ArticleCard';
 import NarrativeCard from '../NarrativeCard';
-import { IArticle, INarrative, FeedItem } from '../../types';
-import * as api from '../../services/api';
+import { IArticle, INarrative } from '../../types';
+import { useRadio } from '../../context/RadioContext';
 
 interface FeedItemRendererProps {
-  item: FeedItem;
-  onOpenNarrative: (narrative: INarrative) => void;
-  onCompare: (article: IArticle) => void;
-  onAnalyze: (article: IArticle) => void;
-  onShare: (article: IArticle) => void;
-  savedArticleIds: Set<string>;
-  onToggleSave: (article: IArticle) => void;
-  showTooltip: (text: string, e: React.MouseEvent) => void;
-  currentArticleId?: string;
-  playSingle: (article: IArticle) => void;
-  stop: () => void;
+  item: IArticle | INarrative;
+  index: number;
+  feedContext: IArticle[]; // Full list for the queue
 }
 
-const FeedItemRenderer: React.FC<FeedItemRendererProps> = React.memo(({
-  item,
-  onOpenNarrative,
-  onCompare,
-  onAnalyze,
-  onShare,
-  savedArticleIds,
-  onToggleSave,
-  showTooltip,
-  currentArticleId,
-  playSingle,
-  stop
-}) => {
-  
-  if (item.type === 'Narrative') {
+const FeedItemRenderer: React.FC<FeedItemRendererProps> = ({ item, index, feedContext }) => {
+  const { startRadio, currentArticle, isPlaying } = useRadio();
+
+  // --- Type Guard ---
+  const isNarrative = (item: any): item is INarrative => {
+    return (item as INarrative).clusterId !== undefined;
+  };
+
+  if (isNarrative(item)) {
     return (
-        <div className="article-card-wrapper">
-            <NarrativeCard 
-                data={item as INarrative}
-                onClick={() => onOpenNarrative(item as INarrative)}
-            />
-        </div>
-    );
-  } else {
-    const article = item as IArticle;
-    return (
-        <div className="article-card-wrapper">
-            <ArticleCard
-                article={article}
-                onCompare={() => onCompare(article)}
-                onAnalyze={onAnalyze}
-                onShare={() => onShare(article)} 
-                onRead={() => {
-                    api.logRead(article._id).catch(err => console.error("Log Read Error:", err));
-                    window.open(article.url, '_blank', 'noopener,noreferrer');
-                }}
-                showTooltip={showTooltip}
-                isSaved={savedArticleIds.has(article._id)}
-                onToggleSave={() => onToggleSave(article)}
-                isPlaying={currentArticleId === article._id}
-                onPlay={() => playSingle(article)}
-                onStop={stop}
-            />
-        </div>
+       <div className="feed-item-wrapper" style={{ marginBottom: '1.5rem' }}>
+          <NarrativeCard narrative={item} />
+       </div>
     );
   }
-});
 
-export default FeedItemRenderer;
+  // --- Article Handling ---
+  const article = item as IArticle;
+  
+  const handlePlayClick = () => {
+      // START RADIO FROM HERE
+      // Options: Skip the "Good Morning" greeting, but Enable the "Up Next" timer for the first transition.
+      startRadio(feedContext, index, { skipGreeting: true, enableFirstTimer: true });
+  };
+
+  const isCurrent = currentArticle?._id === article._id;
+
+  return (
+    <div 
+      className="feed-item-wrapper" 
+      style={{ marginBottom: '1rem' }}
+      id={article._id} // Added ID for Auto-Scroll
+      data-article-id={article._id} // Added Data Attribute for Viewport Detection
+    >
+      <ArticleCard 
+        article={article}
+        onPlay={handlePlayClick}
+        isPlaying={isCurrent && isPlaying}
+        isBuffering={isCurrent && !isPlaying} // Simplification, could link to isLoading
+      />
+    </div>
+  );
+};
+
+export default React.memo(FeedItemRenderer);

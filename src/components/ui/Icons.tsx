6764b9750df2b199a06d4acb7146d78c6220d779
pdkokare1 <pdkokare1@gmail.com>
2026-01-15 19:12:42 +0530

    Update Icons.tsx

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -62,7 +62,7 @@ export const ShareIcon = (props: IconProps) => (
 
 export const CompareIcon = (props: IconProps) => (
   <BaseIcon {...props}>
-    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1-2-2h6"></path>
     <polyline points="15 3 21 3 21 9"></polyline>
     <line x1="10" y1="14" x2="21" y2="3"></line>
   </BaseIcon>
@@ -70,7 +70,7 @@ export const CompareIcon = (props: IconProps) => (
 
 export const ExternalLinkIcon = (props: IconProps) => (
   <BaseIcon {...props}>
-    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1-2-2h6"></path>
     <polyline points="15 3 21 3 21 9"></polyline>
     <line x1="10" y1="14" x2="21" y2="3"></line>
   </BaseIcon>

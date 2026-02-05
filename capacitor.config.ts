b3b9f9901974996acaf629e04c6cee24b4e0ddc3
pdkokare1 <pdkokare1@gmail.com>
2026-02-05 14:11:35 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,28 +1,28 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.narrative.app',
-  appName: 'Narrative',
-  webDir: 'dist',
+  appId: 'in.thegamut.app',
+  appName: 'The Gamut',
+  webDir: 'build',
   server: {
     androidScheme: 'https'
   },
   plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#121212",
+      showSpinner: false,
+      androidSplashResourceName: "splash",
+      iosSplashResourceName: "Default-568h@2x~iphone"
+    },
+    StatusBar: {
+      style: "DARK",
+      backgroundColor: "#121212"
+    },
     Keyboard: {
       resize: 'body',
       style: 'dark',
       resizeOnFullScreen: true,
-    },
-    SplashScreen: {
-      launchShowDuration: 2000,
-      launchAutoHide: true,
-      backgroundColor: "#09090b", // Matches your Zinc-950 background
-      androidSplashResourceName: "splash",
-      showSpinner: false,
-    },
-    StatusBar: {
-      style: 'DARK',
-      overlay: false,
     }
   }
 };

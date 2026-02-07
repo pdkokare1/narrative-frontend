5a0ca4ac2aba37ba8e007fc108b6ce37f0fa6aeb
pdkokare1 <pdkokare1@gmail.com>
2026-02-07 11:54:44 +0530

    Update MainActivity.java

--- android/app/src/main/java/in/thegamut/app/MainActivity.java
+++ android/app/src/main/java/in/thegamut/app/MainActivity.java
@@ -1,5 +1,16 @@
 package in.thegamut.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+// 1. Import the Splash Screen API
+import androidx.core.splashscreen.SplashScreen;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // 2. Initialize the Splash Screen (MUST be before super.onCreate)
+        SplashScreen.installSplashScreen(this);
+        
+        super.onCreate(savedInstanceState);
+    }
+}
